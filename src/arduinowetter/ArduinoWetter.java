5fc2c9bf48c23d61a8a94e83379b635664a0dd87
Eugen <eugen.fischer.android@googlemail.com>
2017-10-05 20:07:50 +0200

    Vorlage Menüleiste hinzugefügt - ohne Funktion

--- src/arduinowetter/ArduinoWetter.java
+++ src/arduinowetter/ArduinoWetter.java
@@ -3,6 +3,9 @@ package arduinowetter;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JMenuBar;
+import javax.swing.JMenu;
+import javax.swing.JMenuItem;
 
 public class ArduinoWetter {
 
@@ -38,7 +41,32 @@ public class ArduinoWetter {
 		frame = new JFrame();
 		frame.setBounds(400, 400, 800, 480);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("ArduinoWetter - super-physik.de");;
+		frame.setTitle("ArduinoWetter - super-physik.de");
+		
+		//Menüleiste
+		JMenuBar menuBar = new JMenuBar();
+		frame.setJMenuBar(menuBar);
+		
+		//Menü - Datei
+		JMenu mnDatei = new JMenu("Datei");
+		menuBar.add(mnDatei);
+		
+		//Menüeinträge - Datei - Einstellungen
+		JMenuItem mneinst = new JMenuItem("Einstellungen");
+		mnDatei.add(mneinst);
+		
+		//Menüeinträge - Datei - Schließen
+		JMenuItem mnexit = new JMenuItem("Schließen");
+		mnDatei.add(mnexit);
+		
+		//Menü - Hilfe
+		JMenu mnHilfe = new JMenu("Hilfe");
+		menuBar.add(mnHilfe);
+		
+		//Menüeinträge - Hilfe - Über
+		JMenuItem mntmber = new JMenuItem("Über");
+		mnHilfe.add(mntmber);;
+		
 	}
 
 }
