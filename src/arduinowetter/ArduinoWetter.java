1ad996560052331b926df022d3d27e40fef16da0
Eugen <eugen.fischer.android@googlemail.com>
2017-10-07 00:07:12 +0200

    Tabs eingeführt, Layouts eingeführt, Auslagerung aus der Mainklasse, Verbesserung der Knöpfeabfragung

--- src/arduinowetter/ArduinoWetter.java
+++ src/arduinowetter/ArduinoWetter.java
@@ -1,135 +1,25 @@
 package arduinowetter;
 
 import java.awt.EventQueue;
-import java.awt.event.MouseEvent;
-import java.awt.event.MouseListener;
-import java.util.HashMap;
-import java.util.Map;
 
-import javax.swing.JFrame;
-import javax.swing.JMenuBar;
-import javax.swing.JMenu;
-import javax.swing.JMenuItem;
 
 public class ArduinoWetter {
 	
 	public static final String VERSIONSNUMMER = "0.1";
 
-	private JFrame frame;
-	//MenuItemMap, dient dazu um Items eine Nummer zuzuweisen, über welche festgestellt wird, welcher Knopf betätigt wurde.
-	//Zuweisung erfolgt in der Methode initialisierung.
-	private static Map<JMenuItem, Integer> menuMap = new HashMap<JMenuItem, Integer>(10);
-
 	/**
-	 * Software starten.
+	 * Launch application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					ArduinoWetter window = new ArduinoWetter();
-					window.frame.setVisible(true);
+					new Hauptfenster();
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
 			}
 		});
 	}
-
-	/**
-	 * Konstructor lässt die Initialisierung laufen
-	 */
-	public ArduinoWetter() {
-		initialisierung();
-	}
-
-	/**
-	 * Initialisierung.
-	 */
-	private void initialisierung() {
-		frame = new JFrame();
-		frame.setBounds(400, 400, 1280, 720);
-		frame.setLocationRelativeTo(null);
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		frame.setTitle("ArduinoWetter - super-physik.de");
-		frame.setResizable(false);
-		
-		//Menüleiste
-		JMenuBar menuBar = new JMenuBar();
-		frame.setJMenuBar(menuBar);
-		
-		//Menü - Datei
-		JMenu mnDatei = new JMenu("Datei");
-		menuBar.add(mnDatei);
-		
-		//Menüeinträge - Datei - Schließen
-		JMenuItem mnexit = new JMenuItem("Schließen");
-		mnDatei.add(mnexit);
-		mnexit.addMouseListener(new ListenerMain());
-		menuMap.put(mnexit, 0);
-		
-		//Menü - Extras
-		JMenu mnExtras = new JMenu("Extras");
-		menuBar.add(mnExtras);
-		
-		//Menüeinträge - Extras - Einstellungen
-		JMenuItem mneinst = new JMenuItem("Einstellungen");
-		mnExtras.add(mneinst);
-		mneinst.addMouseListener(new ListenerMain());
-		menuMap.put(mneinst, 1);
-		
-		//Menü - Hilfe
-		JMenu mnHilfe = new JMenu("Hilfe");
-		menuBar.add(mnHilfe);
-		
-		//Menüeinträge - Hilfe - Über
-		JMenuItem mntmber = new JMenuItem("Über");
-		mnHilfe.add(mntmber);
-		mntmber.addMouseListener(new ListenerMain());
-		menuMap.put(mntmber, 2);
-		
-	}
 	
-	
-	private class ListenerMain implements MouseListener {
-
-		@Override
-		public void mouseClicked(MouseEvent arg0) {
-		}
-
-		@Override
-		public void mouseEntered(MouseEvent arg0) {
-		}
-
-		@Override
-		public void mouseExited(MouseEvent arg0) {
-		}
-
-		@Override
-		public void mousePressed(MouseEvent arg0) {
-		}
-
-		@Override
-		public void mouseReleased(MouseEvent arg0) {
-			
-			JMenuItem quelle = (JMenuItem)arg0.getSource();
-			int itemIndex = menuMap.get(quelle);
-			
-			//Programm Schließen
-			if (itemIndex == 0) {
-				System.exit(0);
-			}
-			//Einstellungen aufrufen
-			else if (itemIndex == 1) {
-				new FensterEinstellungen();
-			}
-			//Über Fenster aufrufen
-			else if (itemIndex == 2) {
-				new FensterUeber();
-			}
-			
-		}
-
-	}
-
 }
---
+++ src/arduinowetter/Hauptfenster.java
@@ -0,0 +1,140 @@
+package arduinowetter;
+
+import java.awt.Color;
+import java.awt.GridLayout;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTabbedPane;
+
+public class Hauptfenster {
+	
+	private JFrame fenster;
+	private JMenuItem mnexit, mneinst, mnueber;
+	
+	private JPanel panel1 = new JPanel();
+	private JPanel panel2 = new JPanel();
+	private JPanel panel3 = new JPanel();
+	private JPanel panel4 = new JPanel();
+	
+
+	/**
+	 * Constructor
+	 */
+	public Hauptfenster() {
+		initialisierung();
+	}
+	
+	
+	/**
+	 * Initialization
+	 */
+	private void initialisierung() {
+		fenster = new JFrame("ArduinoWetter - super-physik.de");
+		fenster.setBounds(400, 400, 1280, 720);
+		fenster.setLocationRelativeTo(null);
+		fenster.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		fenster.setResizable(false);
+		fenster.setVisible(true);
+		
+		//Tab Leiste und Tabs erzeugen
+		JTabbedPane tabLeiste = new JTabbedPane();
+		JPanel tab1 = new JPanel();
+		JPanel tab2 = new JPanel();
+		tabLeiste.addTab("Tab1", tab1);
+		tabLeiste.addTab("Tab2", tab2);
+		
+		//Tab1 Layout und Elemente
+		tab1.setLayout(new GridLayout(2, 2));
+		tab1.add(panel1);
+		tab1.add(panel2);
+		tab1.add(panel3);
+		tab1.add(panel4);
+		panel1.setBackground(new Color(155, 155, 130));
+		
+		//Tab2 Layout und Elemente
+		tab2.add(new JButton("Button des zweiten Tabs"));
+		
+		fenster.add(tabLeiste);
+		
+		
+		//Menüleiste
+		JMenuBar menuBar = new JMenuBar();
+		fenster.setJMenuBar(menuBar);
+		
+		//Menü - Datei
+		JMenu mnDatei = new JMenu("Datei");
+		menuBar.add(mnDatei);
+		
+		//Menüeinträge - Datei - Schließen
+		mnexit = new JMenuItem("Schließen");
+		mnDatei.add(mnexit);
+		mnexit.addMouseListener(new ListenerMain());
+		
+		//Menü - Extras
+		JMenu mnExtras = new JMenu("Extras");
+		menuBar.add(mnExtras);
+		
+		//Menüeinträge - Extras - Einstellungen
+		mneinst = new JMenuItem("Einstellungen");
+		mnExtras.add(mneinst);
+		mneinst.addMouseListener(new ListenerMain());
+		
+		//Menü - Hilfe
+		JMenu mnHilfe = new JMenu("Hilfe");
+		menuBar.add(mnHilfe);
+		
+		//Menüeinträge - Hilfe - Über
+		mnueber = new JMenuItem("Über");
+		mnHilfe.add(mnueber);
+		mnueber.addMouseListener(new ListenerMain());
+		
+	}
+	
+	
+	private class ListenerMain implements MouseListener {
+
+		@Override
+		public void mouseClicked(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mouseEntered(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mouseExited(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mousePressed(MouseEvent arg0) {
+		}
+
+		@Override
+		public void mouseReleased(MouseEvent arg0) {
+			
+			//Programm Schließen
+			if (arg0.getSource() == mnexit) {
+				System.out.println("Test");
+				System.exit(0);
+			}
+			//Einstellungen aufrufen
+			else if (arg0.getSource() == mneinst) {
+				new FensterEinstellungen();
+			}
+			//Über Fenster aufrufen
+			else if (arg0.getSource() == mnueber) {
+				new FensterUeber();
+			}
+			
+		}
+
+	}
+
+}
